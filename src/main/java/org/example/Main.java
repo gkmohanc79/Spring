1efa8d4c290b7f9bd83e4b136fe964787628b9e2
gkmohanc79 <gkmohanc79@gmail.com>
2023-02-06 13:04:13 -0500

    second comment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a;
+        a=10;
+
+        System.out.println("Hello world!"+a);
     }
 }
